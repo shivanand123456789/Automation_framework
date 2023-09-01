904f7ee468c20de9a35456035480b5c269aeba25
DELL <DELL@DELL-PC>
2023-09-01 17:41:54 +0530

    Bould B02

--- Selenium/src/parralleexecution.java
+++ Selenium/src/parralleexecution.java
@@ -8,19 +8,19 @@ public class parralleexecution
 {
 	public WebDriver driver;
 	@Test
-	@Parameter ({"default browser"})
+	//@Parameter ({"default browser"})
 	public void test1(String browser) 
 	{
 	System.setProperty("webdriver.gecko.driver", "./softawares/geckodriver.exe");
 	System.setProperty("webdriver.chrome.driver", "./softawares/chromedriver.exe");
 	if(browser.equals("firefox"))
 	{
-		driver=new ChromeDriver();
+		//driver=new ChromeDriver();
 		driver.get("https://www.google.com");
 	}
 	else 
 	{
-		driver=new ChromeDriver();
+		//driver=new ChromeDriver();
 		driver.get("https://www.google.com");
 	}
 	}
